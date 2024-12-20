f647280f66f5a34f3de8fa37c439927c63b01bde
Yevgeniy Ustinov <yustinov@c1r1s1.42prague.com>
2024-12-20 13:32:00 +0100

    position

---
+++ src/position.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   position.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yustinov <yustinov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/20 13:24:06 by yustinov          #+#    #+#             */
+/*   Updated: 2024/12/20 13:31:06 by yustinov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/* get_position:
+*	Assigns a position to each element of a stack from top to bottom
+*	in ascending order.
+*	Example stack:
+*		value:		 3	 0	 9	 1
+*		index:		[3]	[1]	[4]	[2]
+*		position:	<0>	<1>	<2>	<3>
+*/
+static void	get_position(t_stack **stack)
+{
+	t_stack	*tmp;
+	int		i;
+
+	tmp = *stack;
+	i = 0;
+	while (tmp)
+	{
+		tmp->pos = i;
+		tmp = tmp->next;
+		i++;
+	}
+}
+
+/*
+** Gets the current pos of the lowes index node
+*/
+int	get_lowest_index_position(t_stack **stack)
+{
+	t_stack	*tmp;
+	int		lowest_index;
+	int		lowest_pos;
+
+	tmp = *stack;
+	lowest_index = INT_MAX;
+	get_position(stack);
+	lowest_pos = tmp->pos;
+	while (tmp)
+	{
+		if (tmp->index < lowest_index)
+		{
+			lowest_index = tmp->index;
+			lowest_pos = tmp->pos;
+		}
+		tmp = tmp->next;
+	}
+	return (lowest_pos);
+}

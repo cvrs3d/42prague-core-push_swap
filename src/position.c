0ed6163a3dccff9137732b10356fb5a0e506d09f
Yevgeniy Ustinov <yustinov@c1r1s3.42prague.com>
2024-12-20 13:58:09 +0100

    position

--- src/position.c
+++ src/position.c
@@ -6,7 +6,7 @@
 /*   By: yustinov <yustinov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 13:24:06 by yustinov          #+#    #+#             */
-/*   Updated: 2024/12/20 13:31:06 by yustinov         ###   ########.fr       */
+/*   Updated: 2024/12/20 13:57:19 by yustinov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,4 +58,65 @@ int	get_lowest_index_position(t_stack **stack)
 		tmp = tmp->next;
 	}
 	return (lowest_pos);
+}
+
+/*
+*	Picks the best target position in stack A for the given index of
+*	an element in stack B. First checks if the index of the B element can
+*	be placed somewhere in between elements in stack A, by checking whether
+*	there is an element in stack A with a bigger index. If not, it finds the
+*	element with the smallest index in A and assigns that as the target position.
+*/
+static int	get_target(t_stack **a, int b_idx,
+								int target_idx, int target_pos)
+{
+	t_stack	*tmp_a;
+
+	tmp_a = *a;
+	while (tmp_a)
+	{
+		if (tmp_a->index > b_idx && tmp_a->index < target_idx)
+		{
+			target_idx = tmp_a->index;
+			target_pos = tmp_a->pos;
+		}
+		tmp_a = tmp_a->next;
+	}
+	if (target_idx != INT_MAX)
+		return (target_pos);
+	tmp_a = *a;
+	while (tmp_a)
+	{
+		if (tmp_a->index < target_idx)
+		{
+			target_idx = tmp_a->index;
+			target_pos = tmp_a->pos;
+		}
+		tmp_a = tmp_a->next;
+	}
+	return (target_pos);
+}
+
+/* get_target_position:
+*	Assigns a target position in stack A to each element of stack A.
+*	The target position is the spot the element in B needs to
+*	get to in order to be sorted correctly. This position will
+*	be used to calculate the cost of moving each element to
+*	its target position in stack A.
+*/
+void	get_target_position(t_stack **a, t_stack **b)
+{
+	t_stack	*tmp_b;
+	int		target_pos;
+
+	tmp_b = *b;
+	get_position(a);
+	get_position(b);
+	target_pos = 0;
+	while (tmp_b)
+	{
+		target_pos = get_target(a, tmp_b->index, INT_MAX, target_pos);
+		tmp_b->target_pos = target_pos;
+		tmp_b = tmp_b->next;
+	}
 }
